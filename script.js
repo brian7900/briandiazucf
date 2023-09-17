4d1f40f19976d57c25547d7c2cf3858ba16f028e
brian7900 <124191265+brian7900@users.noreply.github.com>
2023-09-17 13:55:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -69,4 +69,55 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // On submit, show Apple quiz results
     document.getElementById('apple-submit').addEventListener('click', showAppleResults);
+
+
+     const amazonQuestions = [
+        {
+            question: "Which of the following is Amazon's cloud computing service?",
+            options: ["Azure", "CloudDrive", "AWS", "GCP"],
+            answer: 2
+        },
+        // ... other amazon questions
+    ];
+
+    const amazonQuizContainer = document.getElementById('amazon-quiz-container');
+    let amazonOutput = [];
+
+    amazonQuestions.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
+        for (let letter in currentQuestion.options) {
+            answers.push(
+                `<label>
+                    <input type="radio" name="amazon-question${questionNumber}" value="${letter}">
+                    ${currentQuestion.options[letter]}
+                </label><br>`
+            );
+        }
+        amazonOutput.push(
+            `<div class="question">${currentQuestion.question}</div>
+            <div class="answers">${answers.join('')}</div><br>`
+        );
+    });
+
+    amazonQuizContainer.innerHTML = amazonOutput.join('');
+
+    document.getElementById('amazon-submit').addEventListener('click', function() {
+        const answerContainers = amazonQuizContainer.querySelectorAll('.answers');
+        let correct = 0;
+
+        amazonQuestions.forEach((currentQuestion, questionNumber) => {
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=amazon-question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            if (userAnswer == currentQuestion.answer) {
+                correct++;
+                answerContainers[questionNumber].style.color = 'green';
+            } else {
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+
+        alert(`You got ${correct} out of ${amazonQuestions.length} questions right!`);
+    });
 });
